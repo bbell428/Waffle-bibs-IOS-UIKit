822fc0dadfadbe15165a987d09b3a746c5e8ab89
bbell428 <428bbell@gmail.com>
2023-11-16 12:26:01 +0900

    color

--- ios-Uikit/ViewController.swift
+++ ios-Uikit/ViewController.swift
@@ -5,11 +5,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CollctionView1: UICollectionView!
     @IBOutlet weak var Label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        CollctionView1.backgroundColor = UIColor(hexCode: "DADDEA")
         
+        //폰트확인
         for fontFamily in UIFont.familyNames {
                     for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
                         print(fontName)
