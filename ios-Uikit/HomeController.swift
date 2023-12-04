0f0d9b522417372c29185a12f2c75c4bdf3757de
bbell428 <428bbell@gmail.com>
2023-12-05 00:11:59 +0900

    TableView

--- ios-Uikit/HomeController.swift
+++ ios-Uikit/HomeController.swift
@@ -7,25 +7,41 @@
 
 import UIKit
 
-class HomeController: UIViewController {
-
+class HomeController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var sproduct:ProductList! = nil
+    
+    let list: [String] = [
+        "안녕", "나는", "김종혁"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //테이블뷰의 이벤트, 데이터소스 처리
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - 데이터소스
+    //아이템 수 리턴
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
-    */
-
+    // 테이블 셀의 객체 리턴
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ListTableViewCell", for: indexPath) as! ListTableViewCell
+        
+        cell.todoList.text = list[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    //MARK: - 테이블 뷰 마진
+    // 셀의 높이값을 리턴
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
 }
