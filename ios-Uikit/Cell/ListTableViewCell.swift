9868cfaa333cc00ca2cc64acbddfc997e7e69dda
bbell428 <428bbell@gmail.com>
2024-01-10 02:31:51 +0900

    update ( 알림창으로 테스트 성공) - 수정계획

--- ios-Uikit/Cell/ListTableViewCell.swift
+++ ios-Uikit/Cell/ListTableViewCell.swift
@@ -49,7 +49,6 @@ class ListTableViewCell: UITableViewCell {
     }
     
     func alpha1() {
-        print("호출 성공")
         ListBackBtn.alpha = 1
         listBackButtonTapped.alpha = 1
         postBackColor.alpha = 1
@@ -59,7 +58,6 @@ class ListTableViewCell: UITableViewCell {
     }
     
     func alpha0() {
-        print("호출 성공")
         ListBackBtn.alpha = 0
         listBackButtonTapped.alpha = 0
         postBackColor.alpha = 0
@@ -107,6 +105,50 @@ class ListTableViewCell: UITableViewCell {
             }
         }.resume()
         
+    }
+    //MARK: - Update, todo수정
+    func updateContent(id: Int, updatedText: String) {
+        // 서버 업데이트를 위한 URL 문자열
+        let urlString = "http://158.179.166.114:8080/todo/\(id)/update"
+        
+        // URL 객체 생성
+        guard let url = URL(string: urlString) else {
+            print("유효하지 않은 URL")
+            return
+        }
+
+        // URLRequest 생성
+        var request = URLRequest(url: url)
+        request.httpMethod = "PATCH"  // HTTP 메서드를 PATCH로 설정
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")  // 요청 헤더 설정
+
+        // HTTP 요청 바디에 담을 데이터 구성
+        let requestBody: [String: Any] = ["contents": updatedText, "complete_chk": false]
+        
+        do {
+            // 데이터를 JSON 형식으로 인코딩하여 HTTP 요청 바디에 설정
+            let jsonData = try JSONSerialization.data(withJSONObject: requestBody)
+            request.httpBody = jsonData
+        } catch {
+            // JSON 인코딩 오류 처리
+            print("JSON 인코딩 오류: \(error)")
+            return
+        }
+
+        // URLSession을 사용하여 서버와 통신하는 데이터 태스크 생성
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                // 네트워크 오류 처리
+                print("오류: \(error)")
+            } else if let httpResponse = response as? HTTPURLResponse {
+                // 서버 응답 처리
+                print("상태 코드: \(httpResponse.statusCode)")
+                // 여기에서 필요한 경우 응답에 대한 추가 처리를 수행할 수 있습니다.
+            }
+        }
+
+        // 데이터 태스크 실행
+        task.resume()
     }
     
 }
